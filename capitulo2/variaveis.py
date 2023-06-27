0c5054acca78eca8d248623a961c4a207dceaf32
JunioResende <junioresende23@gmail.com>
2023-06-27 07:44:50 -0300

    variaveis

---
+++ capitulo2/variaveis.py
@@ -0,0 +1,9 @@
+#Declaracao das Variaveis"
+base = float(input("Digite o valor da base: "))
+altura = float(input("Digite o valor da altura: "))
+
+#Variavel criada para calcular Area de um Retangulo"
+area = base*altura
+
+#Resultado
+print(f"Valor variavel base = {base}\nValor variavel altura {altura}\nArea do Retangulo = {area}")
