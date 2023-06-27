b547c939199e6fefcce8b7189a232411d58af527
JunioResende <junioresende23@gmail.com>
2023-06-27 07:19:42 -0300

    valores

---
+++ capitulo2/valores.py
@@ -0,0 +1,25 @@
+#Tipos de Valores Numericos
+# Inteiros "int"
+num_int = 2
+
+print(num_int, type(num_int))
+
+# Reais "float"
+num_float = 2.5
+
+print(num_float, type(num_float))
+
+#Strings "textos"
+string_1 = "Junio"
+
+print(string_1, type(string_1))
+
+#Boleanos "True False"
+num_1 = 2
+num_2 = 1
+
+compare = num_1 > num_2
+compare2 = num_1 < num_2
+
+print(compare, type(compare))
+print(compare2, type(compare2))
